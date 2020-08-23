33217c5a63fc2965022c9c6bf3a4eed65622df09
IgorK-XCII <IgorK_XCII@pm.me>
2020-08-23 22:24:21 +0300

    implemented services, post/update requests

---
+++ dotnet-rpg-app/Dtos/Character/UpdateCharacterDto.cs
@@ -0,0 +1,16 @@
+﻿using dotnet_rpg_app.Models;
+
+namespace dotnet_rpg_app.Dtos.Character
+{
+    public class UpdateCharacterDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int Health { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public int Luck { get; set; } = 10;
+        public RpgClasses Class { get; set; } = RpgClasses.Necromancer;
+    }
+}
